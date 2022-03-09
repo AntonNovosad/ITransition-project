a992f5e1477b6f6fb6257c773f1ea514e60e37fd
a.novosad <noobito.2011@gmail.com>
2022-03-09 17:30:39 +0300

    ADMVEDU-402 add names dependencies in Dependencies.kt

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -2,4 +2,12 @@ object Dependencies {
 
   private const val coreKtxVersion = "1.7.0"
   val coreKtx = "androidx.core:core-ktx:$coreKtxVersion"
+
+  val core by lazy { "androidx.core:core-ktx:${Versions.core}" }
+  val appCompat by lazy { "androidx.appcompat:appcompat:${Versions.appCompat}" }
+  val material by lazy { "com.google.android.material:material:${Versions.material}" }
+  val constraintLayout by lazy { "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}" }
+  val jUnit by lazy { "junit:junit:${Versions.jUnit}" }
+  val extJUnit by lazy { "androidx.test.ext:junit:${Versions.extJUnit}" }
+  val espresso by lazy { "androidx.test.espresso:espresso-core:${Versions.espresso}" }
 }
