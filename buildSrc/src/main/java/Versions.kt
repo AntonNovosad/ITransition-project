e0430dc34da74a82648f1b154d067e8747e5fa08
a.novosad <noobito.2011@gmail.com>
2022-03-09 17:28:27 +0300

    ADMVEDU-402 create class Versions.kt

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,10 @@
+object Versions {
+
+    const val core = "1.7.0"
+    const val appCompat = "1.4.1"
+    const val material = "1.5.0"
+    const val constraintLayout = "2.1.3"
+    const val jUnit = "4.13.2"
+    const val extJUnit = "1.1.3"
+    const val espresso = "3.4.0"
+}
