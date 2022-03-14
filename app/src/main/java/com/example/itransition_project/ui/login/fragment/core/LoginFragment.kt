2e5126c48cefc3e2950995642e16353abc66b823
Evgeny Fetskovich <e.fetskovich@itransition.com>
2022-03-14 16:17:25 +0300

    Moved logic to LoginViewModel for validation.

--- app/src/main/java/com/example/itransition_project/ui/login/fragment/core/LoginFragment.kt
+++ app/src/main/java/com/example/itransition_project/ui/login/fragment/core/LoginFragment.kt
@@ -6,8 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.MediatorLiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.itransition_project.R
@@ -16,21 +14,9 @@ import com.example.itransition_project.databinding.LoginFragmentBinding
 class LoginFragment : Fragment(R.layout.login_fragment) {
 
     lateinit var binding: LoginFragmentBinding
-    private lateinit var vm: LoginViewModel
 
-    private val emailLiveData = MutableLiveData<String>() //stateflow all
-    private val passwordLiveData = MutableLiveData<String>()
-    private val isValidLiveData = MediatorLiveData<Boolean>().apply {
-        this.value = false
-
-        addSource(emailLiveData) { email ->
-            val password = passwordLiveData.value
-            this.value = vm.validationForm(email, password)
-        }
-        addSource(passwordLiveData) { password ->
-            val email = emailLiveData.value
-            this.value = vm.validationForm(email, password)
-        }
+    private val viewModel: LoginViewModel by lazy {
+        ViewModelProvider(this).get(LoginViewModel::class.java)
     }
 
     override fun onCreateView(
@@ -48,24 +34,40 @@ class LoginFragment : Fragment(R.layout.login_fragment) {
     ) {
         super.onViewCreated(view, savedInstanceState)
 
-        vm = ViewModelProvider(this).get(LoginViewModel::class.java)
+        observeFields()
 
-        val emailEdit = binding.emailEditText
-        val passwordEdit = binding.passwordEditText
-        val loginButton = binding.buttonLogin
+        binding.emailEditText.addTextChangedListener(
+            onTextChanged = { text, start, before, count ->
+                viewModel.updateEmailText(text?.toString() ?: "")
+            }
+        )
 
-        emailEdit.addTextChangedListener { text ->
-            emailLiveData.value = text.toString()
-        }
-        passwordEdit.addTextChangedListener { text ->
-            passwordLiveData.value = text.toString()
-        }
-        isValidLiveData.observe(viewLifecycleOwner) { isValid ->
-            loginButton.isEnabled = isValid
-        }
+        binding.passwordEditText.addTextChangedListener(
+            onTextChanged = { text, start, before, count ->
+                viewModel.updatePasswordText(text?.toString() ?: "")
+            }
+        )
 
         binding.buttonLogin.setOnClickListener {
             findNavController().navigate(R.id.action_loginFragment_to_mainActivity)
+        }
+    }
+
+    private fun observeFields() {
+        viewModel.emailLiveData.observe(viewLifecycleOwner) { email ->
+            if (binding.emailEditText.text.toString() != email) {
+                binding.emailEditText.setText(email)
+            }
+        }
+
+        viewModel.passwordLiveData.observe(viewLifecycleOwner) { password ->
+            if (binding.passwordEditText.text.toString() != password) {
+                binding.passwordEditText.setText(password)
+            }
+        }
+
+        viewModel.isFormValidLiveData.observe(viewLifecycleOwner) { isValid ->
+            binding.buttonLogin.isEnabled = isValid
         }
     }
 }
--- app/src/main/java/com/example/itransition_project/ui/login/fragment/core/LoginViewModel.kt
+++ app/src/main/java/com/example/itransition_project/ui/login/fragment/core/LoginViewModel.kt
@@ -1,15 +1,43 @@
 package com.example.itransition_project.ui.login.fragment.core
 
-import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.itransition_project.databinding.LoginFragmentBinding
 
 private const val MIN_PASSWORD_LENGTH = 2
 private const val MAX_PASSWORD_LENGTH = 10
 
 class LoginViewModel : ViewModel() {
 
-    fun validationForm(email: String, password: String): Boolean {
+    private val emailMutableLiveData = MutableLiveData<String>()
+    val emailLiveData: LiveData<String> = emailMutableLiveData
+
+    private val passwordMutableLiveData = MutableLiveData<String>()
+    val passwordLiveData: LiveData<String> = passwordMutableLiveData
+
+    val isFormValidLiveData: LiveData<Boolean> = MediatorLiveData<Boolean>().apply {
+        value = false
+
+        addSource(emailLiveData) { email ->
+            val password = passwordMutableLiveData.value ?: ""
+            this.value = validationForm(email, password)
+        }
+        addSource(passwordLiveData) { password ->
+            val email = emailMutableLiveData.value ?: ""
+            this.value = validationForm(email, password)
+        }
+    }
+
+    fun updateEmailText(text: String) {
+        emailMutableLiveData.value = text
+    }
+
+    fun updatePasswordText(text: String) {
+        passwordMutableLiveData.value = text
+    }
+
+    private fun validationForm(email: String, password: String): Boolean {
         val isValidEmail = emptyValidator(email) && emailValidator(email)
         val isValidPassword = emptyValidator(password) && passwordValidator(password)
         return isValidEmail && isValidPassword
