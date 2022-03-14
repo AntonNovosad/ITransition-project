8385622bd5854d316ad588bca0ed35d90f5ac62b
a.novosad <noobito.2011@gmail.com>
2022-03-14 15:38:34 +0300

    ADMVEDU-412 add navigation in LoginFragment.kt

--- app/src/main/java/com/example/itransition_project/ui/splash/SplashScreenActivity.kt
+++ app/src/main/java/com/example/itransition_project/ui/splash/SplashScreenActivity.kt
@@ -4,8 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
-import com.example.itransition_project.MainActivity
 import com.example.itransition_project.R
+import com.example.itransition_project.ui.login.LoginActivity
 
 class SplashScreenActivity : AppCompatActivity() {
 
@@ -15,7 +15,7 @@ class SplashScreenActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash_screen)
 
         Handler().postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
         }, 2000)
